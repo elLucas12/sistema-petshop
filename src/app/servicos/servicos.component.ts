d7e35339cbb0182f9435b0f9894babfe3fdcbea7
elLucas12 <l.zunho@edu.pucrs.br>
2024-03-19 14:00:04 -0300

    Seleção de usuário na aba de serviços

--- src/app/servicos/servicos.component.ts
+++ src/app/servicos/servicos.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+export enum Servico {
+  banho,
+  tosa
+}
+
+export enum Porte {
+  pequeno,
+  grande
+}
 
 @Component({
   selector: 'app-servicos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './servicos.component.html',
   styleUrl: './servicos.component.css'
 })
 export class ServicosComponent {
+  isServicoSelecionado: boolean = false;
+  servicoSelecionado: Servico = Servico.banho;
+  isPorteSelecionado: boolean = false;
+  porteSelecionado: Porte = Porte.pequeno;
 
+  selecaoPorte(porte: Porte): void {
+    this.isPorteSelecionado = true;
+    this.porteSelecionado = porte;
+  }
+
+  selecaoServico(servico: Servico): void {
+    this.isServicoSelecionado = true;
+    this.servicoSelecionado = servico;
+  }
 }

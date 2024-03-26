8086441dae52f989c1abca65f696586923b2737b
elLucas12 <l.zunho@edu.pucrs.br>
2024-03-25 21:24:14 -0300

    Páginas de histórico

--- src/app/area-usuario/historico/historico.component.ts
+++ src/app/area-usuario/historico/historico.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+
+import { Servico } from '../../agendamento';
 
 /** Tipo de histórico/organização a ser exibido pela página de histórico. */
 export enum TipoHistorico {
@@ -11,7 +13,7 @@ export enum TipoHistorico {
 @Component({
   selector: 'app-historico',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './historico.component.html',
   styleUrl: './historico.component.css'
 })
@@ -20,6 +22,9 @@ export class HistoricoComponent implements OnInit {
   /** Armazena o tipo de histórico a ser exibido => ['loja', 'servicos']. */
   tipo: TipoHistorico = TipoHistorico.servicos;
   TipoHistorico = TipoHistorico;
+
+  /** Tipos de serviço */
+  Servico = Servico;
 
   constructor(private router: Router, private route: ActivatedRoute) { }
 

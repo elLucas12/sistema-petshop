a43bf22231f68ab11422044c5b901cf18ad4382a
elLucas12 <l.zunho@edu.pucrs.br>
2024-04-01 17:28:42 -0300

    Revert "Outra configuração static website"
    This reverts commit ccdb16937be26d21c738aa4f7786e1ab4246f9f8.

--- karma.conf.js
+++
@@ -1,9 +0,0 @@
-// karma.conf.js
-restartOnFileChange: true,
-restartOnFileChange: true,
-customLaunchers: {
-  ChromeHeadlessCustom: {
-    base: 'ChromeHeadless',
-    flags: ['--no-sandbox', '--disable-gpu']
-  }
-},
